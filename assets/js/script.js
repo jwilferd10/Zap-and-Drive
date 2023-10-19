cf52e4bb1601f8eb66ad1ddc31b0069991d935d8
Jonathan Wilferd <jwilferd10@yahoo.com>
2023-10-18 20:45:06 -0700

    Updated each code to use arrow functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,73 +26,73 @@ const myIcon = L.icon({
   popupAnchor: [-3, -76]
 });
 
-function initializeMap() {
+const initializeMap = () => {
   mymap = L.map(mapContainer).setView([37.09024, -95.712891], 3);
   const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
   const tileURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
   tiles = L.tileLayer(tileURL, { attribution }).addTo(mymap);
   marker = null;
-}
+};
 
 markerButton.addEventListener('click', function () {
   removeMarker();
 });
 
-function removeMarker() {
+const removeMarker = () => {
   if (marker) {
     mymap.removeLayer(marker);
     mymap.closePopup();
     marker = null;
     initializeMap(); // Reinitialize the map
   }
-}
+};
 
-function updateLocalStorage(key, value) {
-  localStorage.setItem(key, value);
-}
+// const updateLocalStorage = (key, value) => {
+//   localStorage.setItem(key, value);
+// }
 
-function getLocalStorage(key) {
-  return localStorage.getItem(key);
-}
+// function getLocalStorage(key) {
+//   return localStorage.getItem(key);
+// }
 
 buttonSearchEV.addEventListener('click', function () {
   getExactLocation();
 });
 
-function getExactLocation() {
+const getExactLocation = () => {
   if ('geolocation' in navigator) {
     navigator.geolocation.getCurrentPosition(setPosition, showError);
   } else {
     notificationEl.innerHTML = "<p>Browser doesn't support Geolocation</p>";
   }
-}
+};
 
-function setPosition(position) {
+const setPosition = (position) => {
   const { latitude, longitude } = position.coords;
   getGeoLocation(latitude, longitude);
-}
+};
 
-function showError(error) {
+const showError = (error) => {
   notificationEl.innerHTML = `<p>${error.message}</p><br>Just Enter City and State</p>`;
-}
+};
 
-function getGeoLocation(latitude, longitude) {
+const getGeoLocation = (latitude, longitude) => {
   fetch(`https://us1.locationiq.com/v1/reverse.php?key=${LOCATIONIQ_API_KEY}&lat=${latitude}&lon=${longitude}&format=json`)
     .then(response => response.json())
     .then(data => {
       cityInput.innerHTML = data.display_name;
       latitudeInput.innerHTML = data.lat;
       longitudeInput.innerHTML = data.lon;
-      updateLocalStorage('city', data.address.city);
+      // updateLocalStorage('city', data.address.city);
       getChargeStation(data.lat, data.lon);
     })
     .catch(error => console.log('error', error));
-}
+};
 
 buttonSubmit.addEventListener('click', function () {
   removeMarker();
   const stateInput = inputValue1.value;
-  updateLocalStorage('State', stateInput);
+  // updateLocalStorage('State', stateInput);
   if (!isValidState(stateInput)) {
     notificationEl.innerHTML = "<p>Please input valid State!!!</p>";
   } else {
@@ -108,17 +108,17 @@ buttonSubmit.addEventListener('click', function () {
   }
 });
 
-function isValidState(state) {
+const isValidState = (state) => {
   const validStates = "wa|or|ca|ak|nv|id|ut|az|hi|mt|wy|co|nm|nd|sd|ne|ks|ok|tx|mn|ia|mo|ar|la|wi|il|ms|mi|in|ky|tn|al|fl|ga|sc|nc|oh|wv|va|pa|ny|vt|me|nh|ma|ri|ct|nj|de|md|dc";
   return validStates.indexOf(state.toLowerCase() + "|") > -1;
-}
+};
 
-function isValidCity(city) {
+const isValidCity = (city) => {
   const cityRegex = /^[a-zA-z] ?([a-zA-z]|[a-zA-z] )*[a-zA-z]$/;
   return city.match(cityRegex) && city !== "";
-}
+};
 
-function getLocation() {
+const getLocation = () => {
   fetch(`https://us1.locationiq.com/v1/search.php?key=${LOCATIONIQ_API_KEY}&city=${inputValue.value}&state=${inputValue1.value}&country=United States of America&format=json`)
     .then(response => response.json())
     .then(data => {
@@ -128,18 +128,18 @@ function getLocation() {
       getChargeStation(result.lat, result.lon);
     })
     .catch(err => alert("Wrong City"));
-}
+};
 
-function getChargeStation(latitude, longitude) {
+const getChargeStation = (latitude, longitude) => {
   fetch(`https://api.openchargemap.io/v3/poi/?output=json&key=${OPENCHARGE_API_KEY}&latitude=${latitude}&longitude=${longitude}&countrycode=US&maxresults=20&compact=true&verbose=false`)
     .then(response => response.json())
     .then(data => {
       handleChargeStationData(data);
     })
     .catch(error => console.log('error', error));
-}
+};
 
-function handleChargeStationData(data) {
+const handleChargeStationData = (data) => {
   let html = "";
   const state = document.querySelector('.title1');
 
@@ -163,7 +163,7 @@ function handleChargeStationData(data) {
     }
   }
 
-  function getEVMap(clickCity) {
+  const getEVMap = (clickCity) => {
     removeMarker();
     data.forEach(element => {
       const { Latitude, Longitude, AddressLine1, AccessComments } = element.AddressInfo;
@@ -176,6 +176,6 @@ function handleChargeStationData(data) {
       }
     });
   }
-}
+};
 
 initializeMap();
